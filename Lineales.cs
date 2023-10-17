d14383787bb8138b2c316869a10fbb0132977ff7
cono200 <tatattian04@gmail.com>
2023-10-17 14:01:40 -0600

    version que avance mas

--- Lineales.cs
+++ Lineales.cs
@@ -8,30 +8,36 @@ namespace EjerciciosDeBusqueda_SLE
 {
     internal class Lineales
     {
-        public void Ejercicio1()
+        public int Ejercicio1()
         {
             int[] numeros = { 1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 };
-
-
-            Console.WriteLine("Escribe el numero que quieres buscar ");
             int buscar= Convert.ToInt32(Console.ReadLine());
             for (int i = 0; i < numeros.Length; i++)
             {
-                if (numeros[i] != buscar)
+                if (numeros[i] == buscar)
                 {
-                    Console.WriteLine("No existe el numero");
-                    break;
+                    buscar = i;
+
+                    Console.WriteLine($"El numero esta en la posicion {buscar}");
+                    return buscar;
+
+                }
+                else
+                {
+                    Console.WriteLine("No existe ese numero");
+                    return "No existe";
                 }
 
-                else if (numeros[i] == buscar)
-                {
-                    Console.WriteLine($"La posicion donde se encuentra el numero es {buscar}");
-                    break;
-                    //buscar = numeros[i];
-                }
+                return buscar;
+
             }
-
             
+            
+
+
+
+
+
         }
 
 
